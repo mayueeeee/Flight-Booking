cd210bd30208eaf2dc6a39e5691cd0ddadecb21f
Unknown <sarunyu.you@gmail.com>
2017-02-12 02:26:33 +0700

    Create main menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+        int select=0;
+        while (input!=5)
+        {
+            //Show main menu
+            System.out.println("Flight booking System");
+            System.out.println("1: Show flights");
+            System.out.println("2: Booking a flight");
+            System.out.println("3: Edit booking");
+            System.out.println("4: Cancel booking");
+            System.out.println("5: Exit");
+            System.out.print("Enter a choice: ");
+            select=input.nextInt();
+        }
+
     }
 }
