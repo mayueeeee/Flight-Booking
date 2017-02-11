42a84f59f88b8d5ab6532627640b88dc283f902c
Unknown <sarunyu.you@gmail.com>
2017-02-12 01:57:22 +0700

    Add Flight Class

---
+++ src/Flight.java
@@ -0,0 +1,26 @@
+/**
+ * Created by nutno on 12/2/2560.
+ */
+public class Flight {
+
+    private String flight_name;
+    private String departure_time;
+    private String arrival_time;
+    private int unbooking_seat;
+
+    public Flight(String flight_name,String departure_time,String arrival_time){
+
+        this.flight_name = flight_name;
+        this.departure_time = departure_time;
+        this.arrival_time = arrival_time;
+        this.unbooking_seat = 5;
+    }
+
+    public void reserveSeat(int seat){
+        this.unbooking_seat = this.unbooking_seat-seat;
+    }
+
+    public void unreserveSeat(int seat){
+        this.unbooking_seat = this.unbooking_seat+seat;
+    }
+}
